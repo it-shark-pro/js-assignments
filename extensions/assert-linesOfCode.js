9c130370c2721cb55a0e05b00087d6f02b006ada
Eduard_Trutsyk <ediktrutsik@gmail.com>
2018-09-24 13:30:44 +0300

    fixed linesOfCode extension

--- extensions/assert-linesOfCode.js
+++ extensions/assert-linesOfCode.js
@@ -1,17 +1,19 @@
 import assert from 'assert';
 
-function linesOfCode(fn, expected, recommended) {
-  const actualLinesOfCode = fn.toString().split('\n').filter(s => s.trim()).length - 2;
-  const isValid = actualLinesOfCode <= expected;
-  if (actualLinesOfCode <= expected && actualLinesOfCode > recommended) {
-    console.log(`Recommended lines of code for ${fn.name} is ${recommended}`); 
-  }
-  const message = `
-        "${fn.name}" should have maximum ${expected} line(s) of code but it ${actualLinesOfCode}.
-        Recommended lines of code for ${fn.name} is ${recommended}
-  `;
+const exclude = ['', ');'];
 
-  assert.ok(isValid, message);
+function linesOfCode(fn, expected, recommended) {
+  const actualLinesOfCode = fn.toString().split('\n').filter(s => !exclude.includes(s.trim())).length - 2;
+  const isValid = actualLinesOfCode <= expected;
+
+  const messages = [`
+        "${fn.name}" should have maximum ${expected} line(s) of code but it ${actualLinesOfCode}.
+  `];
+  if (recommended && !isValid && actualLinesOfCode > recommended) {
+    messages.push(`Recommended lines of code for ${fn.name} is ${recommended}`);
+  }
+
+  assert.ok(isValid, messages.join('\n'));
 }
 
 assert.linesOfCode = linesOfCode;
