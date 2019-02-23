195ad2d2806725df298aae07fb24743cf0861a73
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2019-02-23 04:27:38 -0500

    task 01-8

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -158,7 +158,7 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-  throw new Error('Not implemented');
+  return str.toUpperCase();
 }
 
 /**
