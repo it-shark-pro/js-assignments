40f70266c1951a7f13cc7f884590156cbcaf5338
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2019-03-06 00:51:39 +0300

    ( (x1 - x2)^2 + (y1 - y2)^ 2 )^(1/2)

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -75,7 +75,7 @@ function getAverage(value1, value2) {
  *   (4, 2) (1, 6) => 5
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  throw new Error('Not implemented');
+  return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 }
 
 /**
