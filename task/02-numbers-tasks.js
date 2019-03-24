632bff9885d3223c4543c6cbebc1aef5d76f9502
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2019-03-24 23:52:22 +0300

    prime number

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -209,7 +209,15 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-  throw new Error('Not implemented');
+  if (n == 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
