80d7a3d52aa046aef8ecb9acfce264f2ed6240e6
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2019-03-24 10:38:17 -0400

    Math.pow & Math.round

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -188,7 +188,8 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-  throw new Error('Not implemented');
+  let p = Math.pow(10, pow);
+  return Math.round(num / p) * p;
 }
 
 /**
