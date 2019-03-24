546325f210c00cb0472ddba8c27a3ec4d54f1b7c
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2019-03-24 23:53:40 +0300

    del let add var

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -212,7 +212,7 @@ function isPrime(n) {
   if (n == 1) {
     return false;
   }
-  for (let i = 2; i * i <= n; i++) {
+  for (var i = 2; i * i <= n; i++) {
     if (n % i == 0) {
       return false;
     }
