d387b83538e4065a213e585ba92ce43fe9303b31
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2019-03-25 02:19:45 +0300

    add spaces

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -281,7 +281,7 @@ function isString(value) {
 function getCardId(value) {
   const card1 = ['♣', '♦', '♥', '♠'],
   card2 = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  return card1.indexOf(value.slice(-1))*13 + card2.indexOf(value.slice(0, -1));
+  return card1.indexOf(value.slice(-1)) * 13 + card2.indexOf(value.slice(0, -1));
 }
 
 module.exports = {
