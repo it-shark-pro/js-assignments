e0b5d700d3154a44696024d7841de3579da34052
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2019-03-25 00:00:43 +0300

    sqrt

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -212,8 +212,10 @@ function isPrime(n) {
   if (n == 1) {
     return false;
   }
-  for (i = 2; i * i <= n; i++) {
-    if (n % i == 0) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      return false;
+    }
   }
   return true;
 }
