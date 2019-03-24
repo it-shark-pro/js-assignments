c25745e12e4b4d56fb83f6860bd2dff65090a142
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2019-03-24 23:55:43 +0300

    del 2 str code

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -213,9 +213,7 @@ function isPrime(n) {
     return false;
   }
   for (i = 2; i * i <= n; i++) {
-    if (n % i == 0) {
-      return false;
-    }
+    if (n % i == 0) return false;
   }
   return true;
 }
