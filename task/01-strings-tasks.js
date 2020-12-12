2cd0024211e293866e1e681353037cb944dd3b03
Vova Pekun <vpekun@gmail.com>
2020-12-12 17:52:38 +0300

    Complete removeFirstOccurrences

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -127,7 +127,7 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value)  {
-  throw new Error('Not implemented');
+  return str.replace(value, '');
 }
 
 /**
