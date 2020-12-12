382b33a6aa03597ec691983675b8bd8ce4363049
Vova Pekun <vpekun@gmail.com>
2020-12-12 17:46:24 +0300

    Complete extractNameFromTemplate

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -66,7 +66,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function  extractNameFromTemplate(value) {
-  throw new Error('Not implemented');
+  return value.slice(7, -1);
 }
 
 
