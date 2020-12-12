c14d518f9ed38c19a4f04793213a378cce7b1678
Vova Pekun <vpekun@gmail.com>
2020-12-12 17:55:14 +0300

    Complete convertToUpperCase

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -157,7 +157,7 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-  throw new Error('Not implemented');
+  return str.toUpperCase();
 }
 
 /**
