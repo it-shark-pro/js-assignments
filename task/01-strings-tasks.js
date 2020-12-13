173affb235c82b9c25d226f29fd24e62b6002844
Vova Pekun <vpekun@gmail.com>
2020-12-13 21:26:29 +0300

    Refactor getCardId to code style rules

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -271,9 +271,9 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-  const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  const suits = ['♣', '♦', '♥', '♠'];
-  return ranks.indexOf(value.slice(0, -1)) + suits.indexOf(value.slice(-1)) * ranks.length;
+  const rank = 'A,2,3,4,5,6,7,8,9,10,J,Q,K'.split(',');
+  const suit = '♣,♦,♥,♠'.split(',');
+  return rank.indexOf(value.slice(0, -1)) + suit.indexOf(value.slice(-1)) * 13;
 }
 
 module.exports = {
