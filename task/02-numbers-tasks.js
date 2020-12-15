f2f3886bac6c41da7996718a485b285408685b42
Vova Pekun <vpekun@gmail.com>
2020-12-15 08:12:03 +0300

    Complete toNumber

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -223,7 +223,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-  throw new Error('Not implemented');
+  return Number.isNaN(Number(value)) ? def : Number(value);
 }
 
 module.exports = {
