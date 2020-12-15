5d3dde82f506b3e03b1839d8a293dea2e35dffe1
Vova Pekun <vpekun@gmail.com>
2020-12-15 17:28:24 +0300

    Complete getDistanceBetweenPoints

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -73,7 +73,7 @@ function getAverage(value1, value2) {
  *   (4, 2) (1, 6) => 5
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  throw new Error('Not implemented');
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
 
 /**
