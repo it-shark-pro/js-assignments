210d66edf4404e8a9a29e2bcae02c42afe078115
Vova Pekun <vpekun@gmail.com>
2020-12-16 23:43:42 +0300

    Complete get3TopItems

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -287,7 +287,7 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
 function get3TopItems(arr) {
-  throw new Error('Not implemented');
+  return arr.sort((a, b) => b - a).slice(0, 3);
 }
 
 
