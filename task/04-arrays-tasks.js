5f377e7b32ba241b5085785d38cf0bca750cfed3
Vova Pekun <vpekun@gmail.com>
2020-12-16 22:10:59 +0300

    Comlete removeFalsyValues

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -117,7 +117,7 @@ function removeFalsyValues(arr) {
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
 function getUpperCaseStrings(arr) {
-  throw new Error('Not implemented');
+  return arr.map(item => item.toUpperCase());
 }
 
 
