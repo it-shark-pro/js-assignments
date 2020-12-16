db04207337ec8ed475be7cba6578c83c8879e9c0
Vova Pekun <vpekun@gmail.com>
2020-12-16 23:56:47 +0300

    Complete getFalsyValuesCount

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -354,7 +354,7 @@ function getItemsSum(arr) {
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
 function getFalsyValuesCount(arr) {
-  throw new Error('Not implemented');
+  return arr.filter(item => !item).length;
 }
 
 /**
