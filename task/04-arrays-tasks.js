72157a1045b3dadc19110177ca6b334131110191
Vova Pekun <vpekun@gmail.com>
2020-12-20 15:11:21 +0300

    Complete selectMany

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -536,7 +536,7 @@ function group(array, keySelector, valueSelector) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-  throw new Error('Not implemented');
+  return arr.map(childrenSelector).reduce((res, cur) => [...res, ...cur]);
 }
 
 
