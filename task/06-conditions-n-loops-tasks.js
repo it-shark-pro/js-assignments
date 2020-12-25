8613d1205f51383c9113c4be469aad95d030448a
Vova Pekun <vpekun@gmail.com>
2020-12-26 00:30:35 +0300

    Complete getFizzBuzz

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -29,7 +29,10 @@
  *
  */
 function getFizzBuzz(num) {
-  throw new Error('Not implemented');
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  else if (num % 5 === 0) return 'Buzz';
+  else if (num % 3 === 0) return 'Fizz';
+  else return num;
 }
 
 
